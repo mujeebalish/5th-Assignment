5d4388162ff3ba7fe09265973456e087a3d63490
mujeebalish <mujeebrehman12122@gmail.com>
2024-11-14 19:56:08 -0800

    new

---
+++ App.js
@@ -0,0 +1,13 @@
+var findCountry = () => {
+    var userInput = document.getElementById("userInp");
+    console.log(userInput.value);
+    
+    
+}
+var inp_Outp = () => {
+    var userInput = document.getElementById("userInp").value;
+    var outPutField = document.getElementById("output-field");
+    outPutField.value = userInput;
+    
+    
+}
